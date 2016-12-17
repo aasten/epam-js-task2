59e2d4d65f61ccdac7b1436e3ddff8d132e73a68
sten <aasten@gmail.com>
2016-12-17 20:56:51 +0300

    debehavior second variant attempt

--- debehaviorize.js
+++ debehaviorize.js
@@ -5,51 +5,55 @@
 /**
 Followed two functions copied from https://gist.github.com/pbakondy/e27b08f20cc34c730355
 */
-function copyObject(orig, deep) {
-    // 1. copy has same prototype as orig
-    var copy = Object.create(Object.getPrototypeOf(orig));
-
-    // 2. copy has all of orig’s properties
-    copyOwnPropertiesFrom(copy, orig, deep);
-
-    return copy;
-}
-function copyOwnPropertiesFrom(target, source, deep) {
-    Object.getOwnPropertyNames(source)
-    .forEach(function(propKey) {
-        var desc = Object.getOwnPropertyDescriptor(source, propKey);
-        Object.defineProperty(target, propKey, desc);
-        if (deep && typeof desc.value === 'object') {
-          target[propKey] = copyObject(source[propKey], deep);
-        }
-    });
-    return target;
-}
+// function copyObject(orig, deep) {
+//     // 1. copy has same prototype as orig
+//     var copy = Object.create(Object.getPrototypeOf(orig));
+//
+//     // 2. copy has all of orig’s properties
+//     copyOwnPropertiesFrom(copy, orig, deep);
+//
+//     return copy;
+// }
+// function copyOwnPropertiesFrom(target, source, deep) {
+//     Object.getOwnPropertyNames(source)
+//     .forEach(function(propKey) {
+//         var desc = Object.getOwnPropertyDescriptor(source, propKey);
+//         Object.defineProperty(target, propKey, desc);
+//         if (deep && typeof desc.value === 'object') {
+//           target[propKey] = copyObject(source[propKey], deep);
+//         }
+//     });
+//     return target;
+// }
 
 
 function debehaviorize(some) {
   if(!(some instanceof Object)) {
     // console.log(some + ' is not an instance of Object, it is ' + typeof some);
-    return some;
+    return [];
   }
   // var ret = JSON.parse(JSON.stringify(some));
-  var ret = copyObject(some,true);
+  // var ret = copyObject(some,true);
+  var ret = [];
   for(var propName in some) {
-    if(ret.hasOwnProperty(propName)) {
+    if(some.hasOwnProperty(propName)) {
       // console.log('loop:' + propName + ' is ' + (typeof ret[propName]));
       if(typeof some[propName] === 'function') {
-        // console.log(propName + ' is a function in ' + ret)
+        console.log(propName + ' is a function in ' + some)
         if(Object.isFrozen(some)) {
           throw ' is frozen, can\'t debehaviorize';
         }
         if(Object.isSealed(some)) {
           throw ' is sealed, can\'t debehaviorize';
         }
-        delete ret[propName];
+        ret.push(some[propName]);
+        console.log(ret);
+        delete some[propName];
       } else {
         // console.log('trying to debehaviorize ' + propName);
         try {
-          ret[propName] = debehaviorize(ret[propName]);
+          // ret[propName] = debehaviorize(ret[propName]);
+          ret.concat(debehaviorize(some[propName]));
         } catch(e) {
           // prepending outer property name
           e = '.' + propName + e;
@@ -68,12 +72,12 @@ var a = { n: 1, a: 'a',
 console.log(a);
 // console.log('"a":' + JSON.stringify(a));
 try {
-var da = debehaviorize(a);
+var behavior = debehaviorize(a);
 console.log('After debehaviorizing');
 console.log('a:');
 console.log(a);
-console.log('da:');
-console.log(da);
+console.log('behavior:');
+console.log(behavior);
 } catch(e) {
   console.log('Exception occured while debehaviorizing ' + JSON.stringify(a) + '\n: ' + e);
 }
